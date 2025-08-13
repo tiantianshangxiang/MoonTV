14fa274750f26db79ab7d979ea53b16c377d4f73
tiantianshangxiang <125104704+tiantianshangxiang@users.noreply.github.com>
2025-08-13 13:13:55 +0800

    Update VersionPanel.tsx

--- src/components/VersionPanel.tsx
+++ src/components/VersionPanel.tsx
@@ -296,13 +296,13 @@ const fetchRemoteChangelog = async () => {
                     </div>
                   </div>
                   <a
-                    href='https://github.com/LunaTechLab/MoonTV'
+                    href='www.520937.xyz'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-yellow-600 hover:bg-yellow-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
                   >
                     <Download className='w-3 h-3 sm:w-4 sm:h-4' />
-                    前往仓库
+                    前往更新
                   </a>
                 </div>
               </div>
